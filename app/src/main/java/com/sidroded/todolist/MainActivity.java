1384d7b5d0c38a4388ec22f4213f59942217e7b6
Nikita <86921169+MasterNikPro@users.noreply.github.com>
2023-02-01 01:47:33 +0200

    Fixed problems Androidx

--- app/src/main/java/com/sidroded/todolist/MainActivity.java
+++ app/src/main/java/com/sidroded/todolist/MainActivity.java
@@ -1,9 +1,11 @@
 package com.sidroded.todolist;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
