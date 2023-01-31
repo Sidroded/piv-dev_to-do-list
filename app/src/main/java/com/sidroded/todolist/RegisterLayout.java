b99d624e5b21199681e7af8b8fd5786b8626b363
Sidroded <deinekindaniil21@gmail.com>
2023-02-01 00:36:51 +0100

    Connect FireBase to RegisterLayout

--- app/src/main/java/com/sidroded/todolist/RegisterLayout.java
+++ app/src/main/java/com/sidroded/todolist/RegisterLayout.java
@@ -1,34 +1,50 @@
 package com.sidroded.todolist;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Objects;
 
 public class RegisterLayout extends AppCompatActivity {
     private String email;
     private String password;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_layout);
+        mAuth = FirebaseAuth.getInstance();
     }
 
     public void checkRegister(View view) {
         EditText emailField = (EditText) findViewById(R.id.editTextTextEmailAddress);
         EditText passwordField = (EditText) findViewById(R.id.editTextTextPassword);
+        email = emailField.getText().toString();
+        password = passwordField.getText().toString();
 
-        if (Objects.nonNull(emailField) && Objects.nonNull(passwordField)) {
-            email = emailField.getText().toString();
-            password = passwordField.getText().toString();
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
 
+                        } else {
 
-        } else {
-
-        }
+                        }
+                    }
+                });
 
     }
 }
