51c3bcecca541bb1bffee52d27f0670783ff11c4
Nikita <86921169+MasterNikPro@users.noreply.github.com>
2023-02-01 01:37:44 +0200

    Fixed problems with v7

--- app/src/main/java/com/sidroded/todolist/RegisterLayout.java
+++ app/src/main/java/com/sidroded/todolist/RegisterLayout.java
@@ -1,12 +1,14 @@
 package com.sidroded.todolist;
 
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
