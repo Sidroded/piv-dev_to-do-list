4e040315ace7f1f26e6b332d6f8a33777d07f299
Nikita <86921169+MasterNikPro@users.noreply.github.com>
2023-02-01 00:40:03 +0200

    Firebase Auth Connection

--- app/src/main/java/com/sidroded/todolist/MainActivity.java
+++ app/src/main/java/com/sidroded/todolist/MainActivity.java
@@ -2,36 +2,12 @@ package com.sidroded.todolist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-
-    private String email;
-    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register_layout);
+        setContentView(R.layout.activity_main);
     }
-
-    public void checkRegister(View view) {
-        EditText emailField = (EditText) findViewById(R.id.editTextTextEmailAddress);
-        EditText passwordField = (EditText) findViewById(R.id.editTextTextPassword);
-
-        if (Objects.nonNull(emailField) && Objects.nonNull(passwordField)) {
-            email = emailField.getText().toString();
-            password = passwordField.getText().toString();
-
-
-        } else {
-
-        }
-
-    }
-
 }
