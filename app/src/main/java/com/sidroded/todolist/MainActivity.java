4f6b4d0f1c4911f76fa8ccc57d9da8b3d9a04d91
Sidroded <deinekindaniil21@gmail.com>
2023-02-01 15:30:14 +0100

    Create auto login.

--- app/src/main/java/com/sidroded/todolist/MainActivity.java
+++ app/src/main/java/com/sidroded/todolist/MainActivity.java
@@ -4,19 +4,40 @@ import android.content.Intent;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+public class MainActivity extends AppCompatActivity {
+private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
     }
 
     public void lol(View view) {
         Intent intent = new Intent(this, LoginLayout.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            currentUser.reload();
+            Toast.makeText(MainActivity.this, "Авторизація успішна", Toast.LENGTH_SHORT).show();
+        } else {
+            Intent intent = new Intent(MainActivity.this, LoginLayout.class);
+            startActivity(intent);
+        }
     }
 }
