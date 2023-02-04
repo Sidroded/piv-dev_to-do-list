3f769ba30db4cf800ae54efe24a756b5ce93cde1
Sidroded <deinekindaniil21@gmail.com>
2023-02-04 16:12:56 +0100

    Add activity view

---
+++ app/src/main/java/com/sidroded/todolist/note/NoteViewActivity.java
@@ -0,0 +1,16 @@
+package com.sidroded.todolist.note;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sidroded.todolist.R;
+
+public class NoteViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_view);
+    }
+}
