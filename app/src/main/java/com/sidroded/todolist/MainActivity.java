555f58e281d5a6ece0aa5acc5c4959f47563ec87
Gyperpupok <58809961+Gyperpupok@users.noreply.github.com>
2023-02-05 19:58:20 +0200

    fix theme

--- app/src/main/java/com/sidroded/todolist/MainActivity.java
+++ app/src/main/java/com/sidroded/todolist/MainActivity.java
@@ -58,7 +58,7 @@ public class MainActivity extends AppCompatActivity implements AddFriendDialog.A
 
 
 
-        
+
         
         toolbar.setTitle(Html.fromHtml("<font face = 'arial-bold' color='" + titleColor + "'>Ваші справи</font>"));
 
