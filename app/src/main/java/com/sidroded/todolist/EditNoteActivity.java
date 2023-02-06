6f160dfdd1f4235283b81faf3244913aca7fea90
Sidroded <deinekindaniil21@gmail.com>
2023-02-06 13:10:00 +0100

    Create edit note

---
+++ app/src/main/java/com/sidroded/todolist/EditNoteActivity.java
@@ -0,0 +1,35 @@
+package com.sidroded.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class EditNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_note);
+    }
+
+    public void setDateEdit(View v) {
+
+    }
+
+    public void setTimeEdit(View v) {
+
+    }
+
+    public void setFileEdit(View v) {
+
+    }
+
+    public void addEdit(View v) {
+
+    }
+
+    public void cancelEdit(View v) {
+
+    }
+}
